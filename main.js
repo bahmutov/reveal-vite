f01f111ca1d47924bdda693d6e84b54647aba176
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-17 07:51:58 -0500

    vite

---
+++ main.js
@@ -0,0 +1,9 @@
+import 'reveal.js/dist/reveal.css'
+// see available themes in the
+// node_modules/reveal.js/dist/theme
+//  beige, black, blood, league, moon, night, serif, simple, ...
+import 'reveal.js/dist/theme/sky.css'
+import Reveal from 'reveal.js'
+
+const deck = new Reveal()
+deck.initialize({ hash: true, slideNumber: true })
