2fa4a642666d6067fe18473800bed141e5dcd6f6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-02-17 08:23:30 -0500

    add public static resource

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ import 'reveal.js/dist/reveal.css'
 // see available themes in the
 // node_modules/reveal.js/dist/theme
 //  beige, black, blood, league, moon, night, serif, simple, ...
-import 'reveal.js/dist/theme/sky.css'
+import 'reveal.js/dist/theme/league.css'
+// our styles to adjust the presentation
+import './styles.css'
 import Reveal from 'reveal.js'
 
 const deck = new Reveal()
